4996a2215bfc8b3529b0a14e5c802ae24f85a731
Niklas Schnelle <schnelle@informatik.uni-freiburg.de>
2017-09-12 11:56:13 +0200

    Echo commands we run

--- build_and_run.sh
+++ build_and_run.sh
@@ -15,11 +15,14 @@ if [ $# -lt 2 ] || [ "$1" != "backend" ] && [ "$1" != "learner" ]; then
 	echo "Usage: $0 [backend|learner] name "
 	exit 1
 fi
-
+echo "-----------------------------------------------------------------"
+echo Executing $DOCKER_CMD build -t "aqqu_$1_$2" -f "Dockerfile.$1" .
 $DOCKER_CMD build -t "aqqu_$1_$2" -f "Dockerfile.$1" .
-
+echo "-----------------------------------------------------------------"
 if [ "$1" == "learner" ]; then
+	echo Executing $DOCKER_CMD run --rm -it --name "aqqu_$1_$2_inst" -v "$(pwd)/$DATA_DIR:/app/data" "aqqu_$1_$2"
 	$DOCKER_CMD run --rm -it --name "aqqu_$1_$2_inst" -v "$(pwd)/$DATA_DIR:/app/data" "aqqu_$1_$2"
 else
+	echo Executing $DOCKER_CMD run --rm -d --name "aqqu_$1_$2_inst" -v "$(pwd)/$DATA_DIR:/app/data" "aqqu_$1_$2"
 	$DOCKER_CMD run --rm -d --name "aqqu_$1_$2_inst" -v "$(pwd)/$DATA_DIR:/app/data" "aqqu_$1_$2"
 fi
