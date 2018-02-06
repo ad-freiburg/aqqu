583e03e06d8410ddcd1ffc94cda8828b989ee1cc
Niklas Schnelle <schnelle@informatik.uni-freiburg.de>
2018-02-06 15:06:39 +0100

    Add docker-compose support, reduce to single Dockerfile

--- build_and_run.sh
+++ build_and_run.sh
@@ -13,41 +13,47 @@ INPUT_DIR="./input"
 WORKDATA_DIR="./data"
 
 if [ $# -lt 2 ] || [ "$1" != "backend" ] && [ "$1" != "learner" ] && [ "$1" != "debug" ]; then
-	echo "Usage: $0 [learner|backend|debug] name [PORT]"
+	echo "Usage: $0 [learner|backend|debug] name ranker [PORT]"
 	exit 1
 fi
 
 PORT=8090
-if [ $# -gt 2 ];then
-	PORT=$3
+if [ $# -gt 3 ];then
+	PORT=$4
 fi
-echo "-----------------------------------------------------------------"
-echo Executing $DOCKER_CMD build
-$DOCKER_CMD build -t "aqqu_$1_$2" --build-arg NAME=$2 --build-arg TENSORFLOW=$TENSORFLOW -f "Dockerfile.$1" .
-echo "-----------------------------------------------------------------"
 
 INPUT_VOLUME="$(pwd)/$INPUT_DIR:/app/input"
 WORKDATA_VOLUME="$(pwd)/$WORKDATA_DIR:/app/data"
 MODELS_VOLUME="aqqu_learner_$2_models_vol:/app/models"
 if [ "$1" == "learner" ]; then
 	echo "Learner"
+	echo "-----------------------------------------------------------------"
+	echo Executing $DOCKER_CMD build
+	$DOCKER_CMD build -t "aqqu_$2" \
+		--build-arg TENSORFLOW=$TENSORFLOW \
+		-f "Dockerfile.base" .
+	echo "-----------------------------------------------------------------"
 	$DOCKER_CMD run --rm -it --name "aqqu_$1_$2_inst" \
 		-v $INPUT_VOLUME \
 		-v $WORKDATA_VOLUME \
 		-v $MODELS_VOLUME \
-		"aqqu_$1_$2"
+		"aqqu_$2" query_translator.learner train $3
 elif [ "$1" == "backend" ]; then
 	echo "Backend"
 	$DOCKER_CMD run --rm -d -p 0.0.0.0:$PORT:8090 --name "aqqu_$1_$2_inst" \
 		-v $INPUT_VOLUME  \
 		-v $WORKDATA_VOLUME \
 		-v $MODELS_VOLUME \
-		"aqqu_$1_$2"
+		"aqqu_$2" translator_server $3
 else
 	echo "Debug"
+	echo Executing $DOCKER_CMD build
+	$DOCKER_CMD build -t "aqqu_debug_$2" \
+		--build-arg TENSORFLOW=$TENSORFLOW \
+		-f "Dockerfile.base" .
 	$DOCKER_CMD run --rm -it -p 0.0.0.0:$PORT:8090 --name "aqqu_$1_$2_inst" \
 		-v $INPUT_VOLUME  \
 		-v $WORKDATA_VOLUME \
 		-v $MODELS_VOLUME \
-		"aqqu_$1_$2"
+		"aqqu_debug_$2" translator_server $3
 fi
